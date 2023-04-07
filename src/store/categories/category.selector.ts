a8c30e592611c134ac2e1aa61ac55bd7de2c006f
binhstatic <binhnv@MECORP.LOCAL>
2023-04-07 08:30:10 +0700

    chore: remove comments

--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -26,13 +26,3 @@ export const selectCategoriesIsLoading = createSelector(
   [selectCategoryReducer],
   (categoriesSlice) => categoriesSlice.isLoading
 );
-
-// export const selectCategoriesMap = (categories: Category[]): CategoryMap =>
-//   categories.reduce((acc, category) => {
-//     const { title, items } = category;
-//     acc[title.toLowerCase()] = items;
-//     return acc;
-//   }, {} as CategoryMap);
-
-// export const selectCategoriesIsLoading = (categoriesSlice: any) =>
-//   categoriesSlice.isLoading;
